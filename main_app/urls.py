65505a4504cbca682dea985404b969352150a7b5
BriannaRocha <rochabrianna17@gmail.com>
2024-03-07 03:53:20 -0500

    add drug detail.html and css, update urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('drugs/', views.drug_index, name='drug-index'),
-  path('drugs/<int:drug_id>/', views.drug_detail, name='drug-detail'),
+  path('medications/', views.drug_index, name='drug-index'),
+  path('medications/<int:drug_id>/', views.drug_detail, name='drug-detail'),
 ]
