1e9618063ef36e0e903460ff27a8ec21ce5d7cd0
BriannaRocha <rochabrianna17@gmail.com>
2024-03-07 03:10:50 -0500

    create drug model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+FORMS = (
+  ('P', 'Pill'),
+  ('C', 'Capsule'),
+  ('T', 'Tablet'),
+  ('L', 'Liquid'),
+  ('I', 'Injection'),
+  ('D', 'Drops'),
+  ('A', 'Topical'),
+  ('S', 'Suppository'),
+  ('W', 'Powder'),
+  ('Y', 'Spray'),
+  ('Z', 'Lozenge'),
+  ('O', 'Other')
+)
+
+class Drug(models.Model):
+  name = models.CharField(max_length=150)
+  form = models.CharField(
+    max_length=1,
+    choices=FORMS,
+    default=FORMS[0][0]
+  )
+  duration = models.CharField(max_length=150)
+  notes = models.TextField(max_length=500)
+
+  def __str__(self):
+    return self.name
