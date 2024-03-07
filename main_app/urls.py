eedf052e425cd5e85dc05ac17bed81467b9e358b
BriannaRocha <rochabrianna17@gmail.com>
2024-03-07 04:35:53 -0500

    add edit and delete functionality to drugs

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('medications/', views.drug_index, name='drug-index'),
   path('medications/<int:drug_id>/', views.drug_detail, name='drug-detail'),
   path('drugs/create/', views.DrugCreate.as_view(), name='drug-create'),
+  path('drugs/<int:pk>/update/', views.DrugUpdate.as_view(), name='drug-update'),
+  path('drugs/<int:pk>/delete/', views.DrugDelete.as_view(), name='drug-delete'),
 ]
