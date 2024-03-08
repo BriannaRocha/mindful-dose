fdf10d16b5dab3af04f2126e29bbe3988fe86f75
BriannaRocha <rochabrianna17@gmail.com>
2024-03-08 00:19:43 -0500

    add time widget for dose form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,11 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Dose
 
 class DoseForm(ModelForm):
   class Meta:
     model = Dose
     fields = ['date', 'dosage', 'time']
+    widgets = { 
+      'time': forms.TimeInput(attrs={'type': 'time'})
+    }
