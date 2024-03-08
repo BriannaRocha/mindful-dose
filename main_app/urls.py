4672fb456b35816392062b4ede95983938939eb3
BriannaRocha <rochabrianna17@gmail.com>
2024-03-07 22:41:48 -0500

    remove dose update and delete for now

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,7 +10,5 @@ urlpatterns = [
   path('medications/<int:pk>/update/', views.DrugUpdate.as_view(), name='drug-update'),
   path('medications/<int:pk>/delete/', views.DrugDelete.as_view(), name='drug-delete'),
   path('medications/<int:drug_id>/add-dose/', views.add_dose, name='add-dose'),
-  path('dose/<int:pk>/update/', views.DoseUpdate.as_view(), name='dose-update'),
-  path('dose/<int:pk>/delete/', views.DoseDelete.as_view(), name='dose-delete'),
   path('accounts/signup/', views.signup, name='signup'),
 ]
